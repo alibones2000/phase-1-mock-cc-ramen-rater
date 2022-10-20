262fbb576fab9c778b23b8a7f4cdc9b01227385a
Ali Eladma <alieladma2000@gmail.com>
2022-10-20 11:46:50 -0400

    mock cc

--- src/index.js
+++ src/index.js
@@ -1,1 +1,52 @@
-// write your code here
+const img = document.querySelector('.detail-image')
+const name = document.querySelector('#ramen-menu')
+const paragraph = document.createElement('p')
+const form = document.getElementById('form')
+
+
+
+
+fetch ('http://localhost:3007/ramens')
+.then( (res) => res.json())
+.then (updateList)
+
+document.getElementById('form').addEventListener
+
+
+
+       
+
+        
+   
+   
+
+
+    
+   
+   
+   
+   
+
+   
+   
+    
+        
+
+        
+      
+
+    
+   
+
+
+
+
+
+
+    
+        
+  
+
+
+
+
